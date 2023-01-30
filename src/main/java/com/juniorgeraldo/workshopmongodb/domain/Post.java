b31abb936ae67332d650c78ae952c1a6d87ab962
Juninho Santos <juninhosantos2016@icloud.com>
2023-01-30 20:33:37 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/juniorgeraldo/workshopmongodb/domain/Post.java
+++ src/main/java/com/juniorgeraldo/workshopmongodb/domain/Post.java
@@ -1,13 +1,16 @@
 package com.juniorgeraldo.workshopmongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.juniorgeraldo.workshopmongodb.dto.AuthorDTO;
+import com.juniorgeraldo.workshopmongodb.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -21,7 +24,9 @@ public class Post implements Serializable {
 	private String body;
 	private AuthorDTO author;
 	
-	
+	private List<CommentDTO> comments = new ArrayList<>();
+
+
 	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
@@ -85,6 +90,15 @@ public class Post implements Serializable {
 		this.author = author;
 	}
 	
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
