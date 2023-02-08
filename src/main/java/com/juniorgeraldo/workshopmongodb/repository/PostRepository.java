5159b4c78bab9078018e7f0aa5e66f4630cea9d9
Juninho Santos <juninhosantos2016@icloud.com>
2023-02-08 11:18:15 -0300

    Consulta simples com query methods

--- src/main/java/com/juniorgeraldo/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/juniorgeraldo/workshopmongodb/repository/PostRepository.java
@@ -3,10 +3,13 @@ package com.juniorgeraldo.workshopmongodb.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 import com.juniorgeraldo.workshopmongodb.domain.Post;
 import com.juniorgeraldo.workshopmongodb.domain.User;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
-
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/juniorgeraldo/workshopmongodb/resources/PostResource.java
+++ src/main/java/com/juniorgeraldo/workshopmongodb/resources/PostResource.java
@@ -1,23 +1,18 @@
 package com.juniorgeraldo.workshopmongodb.resources;
 
-import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
+
+import com.juniorgeraldo.workshopmongodb.domain.Post;
+import com.juniorgeraldo.workshopmongodb.resources.util.URL;
+import com.juniorgeraldo.workshopmongodb.services.PostService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import com.juniorgeraldo.workshopmongodb.domain.Post;
-import com.juniorgeraldo.workshopmongodb.domain.User;
-import com.juniorgeraldo.workshopmongodb.dto.UserDTO;
-import com.juniorgeraldo.workshopmongodb.services.PostService;
-import com.juniorgeraldo.workshopmongodb.services.UserService;
 
 @RestController
 @RequestMapping(value="/posts")
@@ -37,6 +32,13 @@ public class PostResource {
 	public ResponseEntity<Post> findById( @PathVariable String id){
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle( @RequestParam(value = "text", defaultValue = "") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 	/*
 	@RequestMapping(method=RequestMethod.POST)
